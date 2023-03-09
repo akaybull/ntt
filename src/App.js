e1cbd1db1c3324badeeb94e34cef446c5ca2405a
Akay Mehmet Bul <akaybul@gmail.com>
2023-03-09 15:33:27 +0300

    footer

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { Box } from "@mui/material";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import HeaderMenu from "./components/Menu";
+import Sliderr from "./components/Slider";
 function App() {
   return (
     <Box>
       <Navbar />
       <HeaderMenu />
+      <Sliderr />
       <div>asdasd </div>
       <Footer />
     </Box>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,40 +1,118 @@
 import React from "react";
-import { AppBar, Toolbar, Typography, Link, Box } from "@mui/material";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Box,
+  InputBase,
+  Button,
+  Divider,
+} from "@mui/material";
 import styled from "@emotion/styled";
 import {
   FacebookIcon,
   InstagramIcon,
   LinkedInIcon,
   NttVisionIcon,
-  NttIcon,
 } from "../assets/icons";
 const StyledTypography = styled(Typography)({
   fontSize: "20px",
-  font: "Roboto",
   lineHeight: "23.44px",
   color: "#fff",
   display: "flex",
   justifyContent: "center",
 });
+const StyledTitleTypography = styled(Typography)({
+  fontSize: "24px",
+  lineHeight: "28.13px",
+  color: "#fff",
+  fontWeight: "500",
+  display: "flex",
+  justifyContent: "center",
+});
+const StyledButton = styled(Button)({
+  borderRadius: "0px",
 
+  height: "38px",
+  width: "110px",
+  backgroundColor: "#00254F",
+  textTransform: "capitalize",
+});
+const StyledDivider = styled(Divider)({
+  border: "1px solid #D9D9D9",
+});
 const Footer = () => {
   return (
     <Box
       sx={{
-        position: "fixed",
         bottom: 0,
         width: "100%",
         background: "#0059BC",
       }}
     >
-      <AppBar position="static">
-        <Toolbar className="flex flex-col mx-32 py-10">
+      <AppBar position="static" sx={{ background: "#0059BC" }}>
+        <Toolbar className="flex flex-col sm:mx-8 xl:mx-32 py-4">
           <div className="flex w-full">
-            <div className="flex w-2/5 flex-col gap-2 justify-center py-10 ">
-              <NttIcon color="#fff" />
+            <div className="flex py-5 w-3/4 sm:w-1/2  lg:w-1/2 flex-col justify-center sm:py-10 ">
               <NttVisionIcon />
+              <div className="hidden sm:flex lg:w-3/4 sm:w-full mt-6">
+                <Typography>
+                  Lorem ipsum dolor sit amet consectetur. Auctor tempor pretium
+                  aliquam neque.
+                </Typography>
+              </div>
+              <div className="flex w-full lg:w-3/4 mt-6">
+                <div
+                  className="flex w-full justify-between items-center"
+                  style={{
+                    height: "38px",
+                    background: "#FFFFFF",
+                  }}
+                >
+                  <InputBase
+                    fullWidth
+                    className="ml-2"
+                    placeholder="Email"
+                    inputProps={{ "aria-label": "Email" }}
+                  />
+                  <StyledButton variant="contained">Sign Up</StyledButton>
+                </div>
+              </div>
+            </div>
+            <div className="hidden lg:flex lg:justify-start lg:w-1/2">
+              <div className="hidden lg:flex  lg:gap-3 lg:flex-col lg:justify-center lg:items-center lg:w-1/3">
+                <StyledTitleTypography>Title</StyledTitleTypography>
+                <StyledTypography>Subtitle</StyledTypography>
+                <StyledTypography>Subtitle</StyledTypography>
+                <StyledTypography>Subtitle</StyledTypography>
+                <StyledTypography>Subtitle</StyledTypography>
+              </div>
+              <div className="hidden lg:flex  lg:gap-3 lg:flex-col lg:justify-center lg:items-center lg:w-1/3">
+                <StyledTitleTypography>Title</StyledTitleTypography>
+                <StyledTypography>Subtitle</StyledTypography>
+                <StyledTypography>Subtitle</StyledTypography>
+                <StyledTypography>Subtitle</StyledTypography>
+                <StyledTypography>Subtitle</StyledTypography>
+              </div>
+              <div className="hidden lg:flex  lg:gap-3 lg:flex-col lg:justify-center lg:items-center lg:w-1/3">
+                <StyledTitleTypography>Title</StyledTitleTypography>
+                <StyledTypography>Subtitle</StyledTypography>
+                <StyledTypography>Subtitle</StyledTypography>
+                <StyledTypography>Subtitle</StyledTypography>
+                <StyledTypography>Subtitle</StyledTypography>
+              </div>
+            </div>
+            <div className="hidden sm:flex gap-3 flex-col justify-center items-center lg:hidden w-1/3">
+              <StyledTypography>Title</StyledTypography>
+              <StyledTypography>Title</StyledTypography>
+              <StyledTypography>Title</StyledTypography>
+              <StyledTypography>Title</StyledTypography>
+              <StyledTypography>Title</StyledTypography>
             </div>
           </div>
+        </Toolbar>
+        <StyledDivider />
+        <Toolbar className="flex flex-col sm:mx-8 xl:mx-32 py-4">
           <div className="flex w-full justify-between">
             <div className="flex w-1/3 justify-start">
               <StyledTypography>contact@nttdata.com</StyledTypography>
@@ -42,7 +120,7 @@ const Footer = () => {
             <div className="flex w-1/3 justify-center">
               <StyledTypography>+3 9876 765 444</StyledTypography>
             </div>
-            <div className="flex w-1/3 gap-6 justify-end">
+            <div className="hidden sm:flex sm:w-1/3 sm:gap-6 sm:justify-end">
               <InstagramIcon />
               <FacebookIcon />
               <LinkedInIcon />
